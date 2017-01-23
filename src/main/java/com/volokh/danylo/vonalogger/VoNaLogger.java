23a4304820a24da5a4ebfef1b11a6a26803b2780
Danylo Volokh <v.danylo@gmail.com>
2017-01-24 01:05:07 +0200

    Documentation

--- src/main/java/com/volokh/danylo/vonalogger/VoNaLogger.java
+++ src/main/java/com/volokh/danylo/vonalogger/VoNaLogger.java
@@ -7,17 +7,31 @@ public interface VoNaLogger {
 
     void initVoNaLoggerAfterStopping() throws IOException;
 
+    /**
+     * After calling this method logger is no longer valid.
+     * New instance needs to be created.
+     */
+    void releaseResources();
+
     File[] stopLoggingAndGetLogFilesSync();
     File[] processPendingLogsStopAndGetLogFilesSync();
 
     void stopLoggingAndGetLogFiles(GetFilesCallback filesCallback);
     void processPendingLogsStopAndGetLogFiles(GetFilesCallback filesCallback);
 
+    /**
+     * This method returns a files that contain a snapshot of a current files that are being logged.
+     * @return array of snapshot files.
+     */
     File[] getLoggingFilesSnapShotSync();
 
+    /**
+     * This method is used to write log into file.
+     *
+     * @param parameters - array of items that will be separated and interpreted as a simple log entry.
+     * @return error code. 0 if logger is no longer process the parameters.
+     */
     int writeLog(Object... parameters);
-
-    void releaseResources();
 
     class Builder {
 
@@ -27,6 +41,11 @@ public interface VoNaLogger {
 
         private Integer mMinimumEntriesCount;
 
+        /**
+         * This method sets the file name for a logging.
+         * This file name is passed to the concrete constructor - {@link VoNaLoggerImpl}
+         * Calling this method is mandatory
+         */
         public Builder setLoggerFileName(String logFileName){
             if(mLogFileName != null){
                 throw new IllegalArgumentException("logFileName is already set for this Logger. Please create new logger.");
@@ -35,18 +54,27 @@ public interface VoNaLogger {
             return this;
         }
 
+        /**
+         * This method sets the directory in which the log files will be stored.
+         * Calling this method is mandatory
+         */
         public Builder setLoggerFilesDir(File logDir){
-
             this.mLogDir = logDir;
-
             return this;
         }
 
+        /**
+         * This methods sets max size in bytes for the log files.
+         * Calling this method is mandatory
+         */
         public Builder setLogFileMaxSize(long sizeInBytes){
             mLogFileMaxSize = sizeInBytes;
             return this;
         }
 
+        /**
+         * This method creates a concrete class of {@link VoNaLogger}
+         */
         public VoNaLogger build() throws IOException {
 
             checkLogDirNotNull();
